e9ef898c9843068332fddfb19da17d82af6a1672
neilchetty <neilchetty4559@gmail.com>
2022-07-27 09:37:11 +0530

    RMP6768: Fix path for common_full_tablet

--- lineage_RMP6768.mk
+++ lineage_RMP6768.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_tablet.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
 
 # Inherit from RMP6768 device
 $(call inherit-product, device/realme/RMP6768/device.mk)
