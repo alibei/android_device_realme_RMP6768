9ba93a16bf103a4d8dd7d776fb40e6d5d4e952c4
neilchetty <neilchetty4559@gmail.com>
2022-07-28 18:07:45 +0530

    RMP6768: Import IMS and KPOC Blobs from stock

--- device.mk
+++ device.mk
@@ -123,6 +123,16 @@ PRODUCT_PACKAGES += \
 -include $(DEVICE_PATH)/system_prop.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# Telephony Jars
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # RcsService
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
