5984a6abb868b8aedbe14a0d104452d38d50eac5
Sujin Panicker <spanic@codeaurora.org>
2022-09-12 15:42:55 +0530

    RMP6768: Fix rank of SW OMX components Rank OMX SW codecs higher than C2 codecs except for opus codec.
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I49d8917aaaaeb58fe67dc2aaa4a80181eb4ab08b
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -72,3 +72,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # ADB
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0
+
+# OMX
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
+    
