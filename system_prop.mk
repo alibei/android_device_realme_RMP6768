f4df7988e74c95a853a2ebcae704cdf1d8870d55
bohu <bohu@google.com>
2022-09-12 15:44:12 +0530

    RMP6768: Disable codec2
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Iebad5d755b7bcbdfa6d4242b74a5b0cb0b6eaa36
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -77,4 +77,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
-    
+    debug.stagefright.ccodec=0
