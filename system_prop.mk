627971b06606f7fea833e683ced5a104c6537017
techyminati <sinha.aryan03@gmail.com>
2022-09-18 19:04:43 +0530

    RMP6768: Add props for memory management
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -97,3 +97,15 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.egl.profiler=1 \
     debug.egl.hw=1 \
     debug.composition.type=gpu
+
+# Tweak the memory management of the device, enable more background apps.. etc..
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
