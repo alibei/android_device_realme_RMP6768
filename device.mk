daef9844ef16525e89ef8539a76648c46984425f
SamarV-121 <samarvispute121@pm.me>
2022-10-05 08:19:03 +0000

    RMP6768: Fix booting of aosp recovery
    Co-authored-by:  neilchetty <neilchetty4559@gmail.com>

--- device.mk
+++ device.mk
@@ -98,8 +98,7 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     init.mt6768.rc \
-    init.recovery.mt6768.sh \
-    init.safailnet.rc \
+    init.safailnet.rc
 
 # Tablet
 PRODUCT_CHARACTERISTICS := tablet
