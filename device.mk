b57dbeec5dd07f6134c4b95f7ddbd0d66469e68f
neilchetty <neilchetty4559@gmail.com>
2022-10-26 14:32:46 +0530

    RMP6768: Remove Mtkincallservice

--- device.mk
+++ device.mk
@@ -114,10 +114,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ImsServiceBase
 
-# MTKInCallService
-PRODUCT_PACKAGES += \
-    MtkInCallService
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
