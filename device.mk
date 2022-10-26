7a7c2f887078c5408d0083a9fbf18bd3b64ffcee
neilchetty <neilchetty4559@gmail.com>
2022-10-26 14:36:58 +0530

    RMP6768: Build textclassifer

--- device.mk
+++ device.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.memtrack@1.0-impl
 
+# Textclassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_hash.vendor
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
