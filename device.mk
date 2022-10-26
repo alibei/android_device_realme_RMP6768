ce2f4b42259f46401394eaf86bc096b715e4c8ff
neilchetty <neilchetty4559@gmail.com>
2022-10-26 14:43:28 +0530

    RMP6768: Add logspam prop

--- device.mk
+++ device.mk
@@ -164,6 +164,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Log Spams
+-include $(DEVICE_PATH)/configs/props/vendor_logging_prop.mk
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
 # Fastboot
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
