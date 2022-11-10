864bce30ac3d2942048c3a962689df5821309f43
neilchetty <neilchetty4559@gmail.com>
2022-11-10 20:08:17 +0530

    RMP6768: sepolicy: label light service
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,6 +178,7 @@ VENDOR_SECURITY_PATCH := 2022-09-05
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Properties
