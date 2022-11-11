284e6b24e0d0067852e1520ef73ccc07fcd460fd
SamarV-121 <samarvispute121@pm.me>
2022-11-11 15:38:01 +0530

    RMP6768: Patch libMtkOmxVdecEx to use libui-v32  * Fixes OMX crash https://gist.githubusercontent.com/SamarV-121/efbeed785308c09655c354362b0f007c/raw
    Change-Id: Id7f221f109e27a4d5544aa81ec73c60db0c0dc33
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup {
         vendor/lib*/hw/dfps.mt6768.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
+        vendor/lib/libMtkOmxVdecEx.so)
+            "$PATCHELF" --replace-needed "libui.so" "libui-v32.so" "$2"
+            ;;
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.6-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
