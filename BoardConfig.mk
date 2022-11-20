a07111b0ba772df156342063e81b899901e1532a
neilchetty <neilchetty4559@gmail.com>
2022-11-21 00:01:11 +0530

    RMP6768: Set selinux to enforcing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_KERNEL_BASE := 0x40078000
