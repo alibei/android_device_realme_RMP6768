2e14d26b036f869a16da2d242cbf21abacd570d6
bengris32 <bengris32@protonmail.ch>
2022-12-06 17:09:39 +0530

    RMP6768: vndk: Copy 32 and 64bit variant of libutils

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -2,10 +2,12 @@ LOCAL_PATH := prebuilts/vndk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libutils-v32
-LOCAL_SRC_FILES := v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_arm := v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_MODULE_TARGET_ARCH := arm arm64
 LOCAL_MODULE_TAGS := optional
 LOCAL_CHECK_ELF_FILES := false
 LOCAL_VENDOR_MODULE := true
@@ -13,10 +15,12 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libutils-v30
-LOCAL_SRC_FILES := v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_MULTILIB := both
+LOCAL_SRC_FILES_arm := v30/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_SRC_FILES_arm64 := v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_MODULE_TARGET_ARCH := arm arm64
 LOCAL_MODULE_TAGS := optional
 LOCAL_CHECK_ELF_FILES := false
 LOCAL_VENDOR_MODULE := true
