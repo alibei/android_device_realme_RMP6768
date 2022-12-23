305d5bbd03ce4501b6bceab2cab4f759b9adb86b
Bharath <bharath@teamb58.org>
2022-12-23 15:38:28 +0530

    RMP6768: props: Replace obsolete vendor properties flag
    * PRODUCT_PROPERTY_OVERRIDES & PRODUCT_DEFAULT_PROPERTY_OVERRIDES
    to be replaced with PRODUCT_VENDOR_PROPERTIES.
    
    Change-Id: I6be5a221a146c6ba2ede88a2e70e952015bc477d

--- configs/props/logtag.mk
+++ configs/props/logtag.mk
@@ -4,7 +4,7 @@ else
 VENDOR_LOG_LEVEL=S
 endif
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.NetAgentService=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.NetworkStats=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.RfxSocketSM=$(VENDOR_LOG_LEVEL) \
