670c94109ce9cd69e1d89a67c6261eea9742b387
neilchetty <neilchetty4559@gmail.com>
2022-12-31 08:43:58 +0530

    RMP6768: Build system and recovery health packages
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- device.mk
+++ device.mk
@@ -369,7 +369,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+   android.hardware.health@2.1-impl.recovery \
    android.hardware.health@2.1-service \
+   android.hardware.health@2.0 \
    android.hardware.health@2.1-impl
 
 # Power
