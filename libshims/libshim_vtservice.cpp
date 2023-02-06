ac945081923687efcfb012e6ac3967d9f55aec75
bengris32 <bengris32@protonmail.ch>
2023-02-06 15:43:12 +0530

    RMP6768: libshims: Update vtservice shim from mt6768-dev
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -5,7 +5,6 @@
 using namespace android;
 
 extern "C" {
-
 void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
         void* thisptr,
         audio_stream_type_t streamType,
@@ -46,14 +45,14 @@ void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_o
         bool doNotReconnect,
         float maxRequiredSpeed,
         audio_port_handle_t selectedDeviceId) {
-        AttributionSourceState attributionSource = AttributionSourceState();
-        attributionSource.uid = uid;
-        attributionSource.pid = pid;
-        _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
+    AttributionSourceState attributionSource;
+    attributionSource.uid = uid;
+    attributionSource.pid = pid;
+    attributionSource.packageName = "com.mediatek.ims";
+    _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
                             thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
                             user, notificationFrames, sessionId, transferType, offloadInfo,
-                            attributionSource, pAttributes,
-                            doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+                            attributionSource, pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
 }
 
 void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
