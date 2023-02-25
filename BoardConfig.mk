94d4eb676ed3aa8d7a9dfa7cb292350ba05773c7
Tim Zimmermann <tim@linux4.de>
2023-02-25 08:19:56 +0100

    RMP6768: Reorganize and clean proprietary list
    Also Make all elf prebuilts packages
    
     * To get rid of BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES
    
    Co-authored-by: neilchetty <neilchetty4559@gmail.com>
    Change-Id: I29901e3acd4eb3f9ea1a7d46f2d27b220007c0b9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,11 +16,6 @@
 
 DEVICE_PATH := device/realme/RMP6768
 
-# Build Rules
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_PREBUILT_ELF_FILES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
