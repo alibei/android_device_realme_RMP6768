defb0471322e793ad35ec2f0391a21c341b749b3
alibei <sebastiankotzias83@gmail.com>
2023-04-29 23:29:59 +0200

    RMP6768: Build widevine drm hal from source
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>

--- device.mk
+++ device.mk
@@ -143,7 +143,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4-service.widevine \
+    android.hardware.drm@1.4.vendor
 
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
