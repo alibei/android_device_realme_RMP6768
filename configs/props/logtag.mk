b4478dc599d40c6b8f57d600a9fb5d3d8dd12f19
alibei <sebastiankotzias83@gmail.com>
2023-04-29 03:26:32 +0200

    RMP6768: props: Silence SpeechMessengerNormal logspam
    The audio hal on the wifi variant spams the log with:
    SpeechMessengerNormal: openCcciDriver(), open(/dev/ccci_aud) fail!! mCcciDeviceHandler: -1, errno: 2
    
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>

--- configs/props/logtag.mk
+++ configs/props/logtag.mk
@@ -176,6 +176,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.SimSwitchOP02=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SimSwitchOP18=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SlotQueueEntry=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.SpeechMessengerNormal=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SpnOverride=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.Telecom=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.UiccCard=$(VENDOR_LOG_LEVEL) \
