e90bd21141981db5a5509ccbc984d71a2dda1407
alibei <sebastiankotzias83@gmail.com>
2023-10-22 18:48:28 +0200

    RMP6768: configs: media: Drop google omx video codecs
    * Mtk omx does cover all of them
    
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>

--- device.mk
+++ device.mk
@@ -228,8 +228,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/mtk_omx_core.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_omx_core.cfg
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml
 
 # Memtrack
 PRODUCT_PACKAGES += \
